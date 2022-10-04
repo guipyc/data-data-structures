900f71dc6b4bce2ff83cc017822369aff1ed2593
Guilherme Barreto <guilhermeofficial.35@gmail.com>
2022-10-04 16:40:35 -0300

    solved

--- currencies.py
+++ currencies.py
@@ -1,11 +1,22 @@
 # pylint: disable=missing-docstring
 
-# TODO: add some currency rates
-RATES = {}
+# TODO:
+RATES = {
+    "USDEUR": 0.85,
+    "GBPEUR": 1.13,
+    'CHFEUR': 0.86,
+    'EURGBP': 0.885
+    }
 
 def convert(amount, currency):
     """returns the converted amount in the given currency
     amount is a tuple like (100, "EUR")
     currency is a string
     """
-    pass  # YOUR CODE HERE
+    for key, value in RATES.items():
+
+        if amount[1] == key[0:3] and currency == "EUR":
+            return round(amount[0] * value)
+
+        if amount[1] == key[3:6] and currency == key[0:3]:
+            return round(amount[0] / value)
